b0ff031e47540fcefe18cd0a0329b9952894476a
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-04-19 17:34:10 -0700

    Add: pacific-atlantic-water-flow - submission-7

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-7.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {number[][]} heights
+     * @return {number[][]}
+     */
+    pacificAtlantic(heights) {
+        const atl = new Set();
+        const pac = new Set();
+        const ROWS = heights.length;
+        const COLS = heights[0].length;
+
+        function spread(r, c, prevHeight, visited) {
+            if(r < 0 || c < 0 || r >= ROWS || c >= COLS || heights[r][c] < prevHeight) return;
+            const key = r + "," + c;
+            if(visited.has(key)) return;
+            visited.add(key);
+            spread(r+1, c, heights[r][c], visited);
+            spread(r-1, c, heights[r][c], visited);
+            spread(r, c+1, heights[r][c], visited);
+            spread(r, c-1, heights[r][c], visited);
+        }
+
+        for(let r=0; r < ROWS; r++) {
+            for(let c=0; c < COLS; c++) {
+                spread(0, c, heights[0][c], pac);
+                spread(r, 0, heights[r][0], pac);
+                spread(r, COLS - 1, heights[r][COLS - 1], atl);
+                spread(ROWS - 1, c, heights[ROWS - 1][c], atl);
+            }
+        }
+
+        const res = [];
+
+        for(let r=0; r < ROWS; r++) {
+            for(let c=0; c < COLS; c++) {
+                const key = r + "," + c;
+                if(atl.has(key) && pac.has(key)) res.push([r,c]);
+            }
+        }
+
+        return res;
+    }
+}
