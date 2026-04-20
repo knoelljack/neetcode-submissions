eebcbcee2fce599a6ee1b36bd5d25fb9beec8ee5
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-04-20 12:38:13 -0700

    Add: count-paths - submission-9

---
+++ Data Structures & Algorithms/count-paths/submission-9.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number} m
+     * @param {number} n
+     * @return {number}
+     */
+    uniquePaths(m, n) {
+
+        const memo = {};
+
+        function search(x,y) {
+            if(x < 0 || y < 0 || x >= m || y >= n) return 0;
+            if(x === m - 1 && y === n - 1) return 1;
+            const key = x + "," + y;
+            if(key in memo) return memo[key];
+            memo[key] = search(x+1, y) + search(x, y + 1);
+            return memo[key];
+        }
+
+        return search(0, 0);
+    }
+}
