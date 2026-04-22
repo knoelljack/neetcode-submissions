9fddbab230732b96d2908bcd5dd9c69146b4169b
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-04-22 12:06:31 -0700

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const stack = [];
+
+        const map = {
+            "{" : "}",
+            "[" : "]",
+            "(" : ")"
+        }
+
+        for(let char of s) {
+            if(char in map) stack.push(map[char]);
+            else if(stack.pop() !== char) return false;
+        }
+
+        return stack.length === 0;
+    }
+}
