83a6efe5fc6b572f434f660c45d79b834edafba6
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-04-23 13:48:49 -0700

    Add: house-robber-ii - submission-6

---
+++ Data Structures & Algorithms/house-robber-ii/submission-6.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+        return nums.length === 1 ? nums[0] : Math.max(this.choose(nums.slice(1)), this.choose(nums.slice(0, nums.length - 1)));
+    }
+
+    choose(nums) {
+        let rob1 = 0, rob2 = 0;
+
+        for(let num of nums) {
+            let temp = Math.max(rob1 + num, rob2);
+            rob1 = rob2;
+            rob2 = temp;
+        }
+
+        return rob2;
+    }
+    
+}
