a31f974d16cdad143805861677dfb51cbfaccf35
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-04-23 19:33:09 -0700

    Add: longest-increasing-subsequence - submission-4

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-4.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    lengthOfLIS(nums) {
+
+        const memo = {};
+
+        function dfs(prev, i) {
+            if(i === nums.length) return 0;
+
+            const key = prev + "," + i;
+            if(memo[key]) return memo[key];
+
+            let skip = dfs(prev, i+1);
+
+            let take = 0;
+
+            if(nums[i] > prev) {
+                take = 1 + dfs(nums[i], i + 1);
+            }
+
+            let res = Math.max(skip, take);
+            memo[key] = res;
+
+            return res;
+        }
+
+        return dfs(-Infinity, 0);
+    }
+}
