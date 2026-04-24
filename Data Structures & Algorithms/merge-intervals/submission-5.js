9f4880823f75b8c79c33999b5a6834a560647bc0
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-04-24 10:08:41 -0700

    Add: merge-intervals - submission-5

---
+++ Data Structures & Algorithms/merge-intervals/submission-5.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number[][]}
+     */
+    merge(intervals) {
+        intervals.sort((a,b) => a[0] - b[0]);
+        const res = [];
+
+        let curr = intervals[0];
+
+        for(let i=1; i < intervals.length; i++) {
+            let [start,end] = intervals[i];
+
+            if(start <= curr[1]) {
+                curr[1] = Math.max(curr[1], end);
+            } else {
+                res.push(curr);
+                curr = intervals[i];
+            }
+        }
+
+        res.push(curr);
+
+        return res;
+    }
+}
