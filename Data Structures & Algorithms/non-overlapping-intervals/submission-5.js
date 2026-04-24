233698cd8774d44ec42ae3948a5a2ca2b4356b94
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-04-24 10:28:06 -0700

    Add: non-overlapping-intervals - submission-5

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-5.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number}
+     */
+    eraseOverlapIntervals(intervals) {
+        intervals.sort((a,b) => a[0] - b[0]);
+        let removed = 0;
+        let prevEnd = intervals[0][1];
+
+        for(let i=1; i < intervals.length; i++) {
+            const [start,end] = intervals[i];
+
+            if(start < prevEnd) {
+                prevEnd = Math.min(end, prevEnd);
+                removed++;
+            } else {
+                prevEnd = end;
+            }
+        }
+
+        return removed;
+    }
+}
