78f4136525199ce9d5891ad1bec296643e30da6e
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-04-27 08:29:41 -0700

    Add: is-anagram - submission-16

---
+++ Data Structures & Algorithms/is-anagram/submission-16.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length) return false;
+        s = s.toLowerCase();
+        t = t.toLowerCase();
+        const counts = {};
+
+        for(let char of s) {
+            counts[char] = (counts[char] || 0) + 1;
+        }
+
+        for(let char of t) {
+            if(!counts[char]) return false;
+            counts[char]--;
+        }
+
+        for(let value of Object.values(counts)) {
+            if(value > 0) return false;
+        }
+
+        return true;
+    }
+}
