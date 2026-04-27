9cdfb1887ce961f8e12c4d1ceb1863bbd18abf7e
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-04-27 08:41:53 -0700

    Add: anagram-groups - submission-15

---
+++ Data Structures & Algorithms/anagram-groups/submission-15.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const groups = {};
+
+        for(let str of strs) {
+            const buckets = Array(26).fill(0);
+
+            for(let char of str) {
+                const bucket = char.charCodeAt(0) - "a".charCodeAt(0);
+                buckets[bucket]++;
+            }
+            if(!groups[buckets]) groups[buckets] = [];
+            groups[buckets].push(str);
+
+        }
+
+        return Object.values(groups);
+    }
+}
