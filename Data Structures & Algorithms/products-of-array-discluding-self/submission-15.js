141f96e5ed91bd167c995c9ff14e177bb678c8d2
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-04-29 14:33:31 -0700

    Add: products-of-array-discluding-self - submission-15

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-15.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let pre = 1, suff = 1;
+        const res = Array(nums.length).fill(1);
+
+        for(let i=0; i < nums.length; i++) {
+            res[i] *= pre;
+            pre *= nums[i];
+        }
+
+        for(let i=nums.length - 1; i >= 0; i--) {
+            res[i] *= suff;
+            suff *= nums[i];
+        }
+
+        return res;
+    }
+}
