6712d70a64e5c11a7041d29ce0375f305f5f2bcb
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-04-29 15:03:52 -0700

    Add: validate-parentheses - submission-13

---
+++ Data Structures & Algorithms/validate-parentheses/submission-13.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        if(s.length % 2) return false;
+        const map = {
+            "{" : "}",
+            "[" : "]",
+            "(" : ")"
+        }
+
+        const stack = [];
+
+        for(let char of s) {
+            if(char in map) stack.push(map[char]);
+            else if(stack.pop() !== char) return false;
+        }
+
+        return !stack.length;
+    }
+}
