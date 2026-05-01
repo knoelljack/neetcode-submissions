d5250f09858313685336ac8e74cd6cf3709dd515
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-05-01 09:36:19 -0700

    Add: count-connected-components - submission-6

---
+++ Data Structures & Algorithms/count-connected-components/submission-6.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {number}
+     */
+    countComponents(n, edges) {
+        const adj = Array.from({length: n}, () => []);
+        const visited = new Set();
+        let count = 0;
+
+        for(let [n1, n2] of edges) {
+            adj[n1].push(n2);
+            adj[n2].push(n1);
+        }
+
+        function visit(node) {
+            if(visited.has(node)) return;
+            visited.add(node);
+
+            for(let nei of adj[node]) {
+                visit(nei);
+            }
+        }
+
+        for(let i=0; i < n; i++) {
+            if(!visited.has(i)) {
+                visit(i);
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
