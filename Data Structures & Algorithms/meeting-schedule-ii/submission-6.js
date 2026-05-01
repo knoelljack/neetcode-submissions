4f4899957e0a56166e6097f78f615feff4780108
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-05-01 15:24:57 -0700

    Add: meeting-schedule-ii - submission-6

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-6.js
@@ -0,0 +1,36 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {number}
+     */
+    minMeetingRooms(intervals) {
+        const starts = intervals.map((i) => i.start).sort((a,b) => a - b);
+        const ends = intervals.map((i) => i.end).sort((a,b) => a - b);
+
+        let s = 0, e = 0;
+        let rooms = 0, maxRooms = 0;
+
+        while(s < starts.length) {
+            if(starts[s] < ends[e]) {
+                rooms++;
+                s++;
+            } else {
+                e++;
+                rooms--;
+            }
+            maxRooms = Math.max(maxRooms, rooms);
+        }
+
+        return maxRooms;
+    }
+}
