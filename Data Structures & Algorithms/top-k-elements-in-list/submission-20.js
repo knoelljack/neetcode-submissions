85bdb7d4afa2e96766a6bbc2eeae71fe8054ffd5
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-05-02 12:41:15 -0700

    Add: top-k-elements-in-list - submission-20

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-20.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const counts = {};
+        const buckets = Array.from({length: nums.length + 1}, () => []);
+        const res = [];
+
+        for(let num of nums) {
+            counts[num] = (counts[num] || 0) + 1;
+        }
+
+        for(let [num,cnt] of Object.entries(counts)) {
+            buckets[cnt].push(num);
+        }
+
+        for(let i=buckets.length - 1; i >= 0; i--) {
+            res.push(...buckets[i]);
+        }
+
+        return res.slice(0,k);
+    }
+}
