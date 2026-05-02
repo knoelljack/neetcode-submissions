c55d1c0ad599edd71dcb93b6b2fb806f9780336b
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-05-02 15:26:41 -0700

    Add: longest-repeating-substring-with-replacement - submission-15

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-15.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let longest = 0;
+        let maxF = 0;
+        let l = 0, r = 0;
+        const counts = {};
+
+        while(r < s.length) {
+            const rightChar = s[r];
+            counts[rightChar] = (counts[rightChar] || 0) + 1;
+            maxF = Math.max(maxF, counts[rightChar]);
+
+            while(r - l + 1 - maxF > k) {
+                const leftChar = s[l];
+                counts[leftChar]--;
+                l++;
+            }
+
+            longest = Math.max(longest, r - l + 1);
+            r++;
+        }
+
+        return longest;
+    }
+}
