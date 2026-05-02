c72a6dc31539076b593d323d232791d55db2d38b
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-05-02 14:40:22 -0700

    Add: longest-consecutive-sequence - submission-19

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-19.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        const set = new Set(nums);
+        let longest = 0;
+
+        for(let num of nums) {
+            let streak = 0;
+            while(set.has(num + streak)) streak++;
+            longest = Math.max(longest, streak);
+        }
+
+        return longest;
+    }
+}
