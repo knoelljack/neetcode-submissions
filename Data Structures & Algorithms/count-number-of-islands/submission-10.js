fa941ed8f35d92b2db4b4eec8d563f4dec1f30c5
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-05-04 09:56:54 -0700

    Add: count-number-of-islands - submission-10

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-10.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {character[][]} grid
+     * @return {number}
+     */
+    numIslands(grid) {
+        const ROWS = grid.length;
+        const COLS = grid[0].length;
+
+        let count = 0;
+
+        function spread(r, c) {
+            if(r < 0 || c < 0 || r >= ROWS || c >= COLS || grid[r][c] !== "1") return;
+            grid[r][c] = "0";
+            spread(r+1, c);
+            spread(r-1, c);
+            spread(r, c+1);
+            spread(r, c-1);
+        }
+
+        for(let r=0; r < ROWS; r++) {
+            for(let c=0; c < COLS; c++) {
+                if(grid[r][c] === "1") {
+                    spread(r, c);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
