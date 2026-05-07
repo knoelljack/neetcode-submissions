b99763bdbf38a38b8d26c21a0c8c1c589850eb28
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-05-07 13:31:32 -0700

    Add: design-word-search-data-structure - submission-21

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-21.js
@@ -0,0 +1,54 @@
+class TrieNode {
+    constructor() {
+        this.children = {};
+        this.word = false;
+    }
+}
+
+class WordDictionary {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+    addWord(word) {
+        let curr = this.root;
+
+        for(let char of word) {
+            if(!curr.children[char]) curr.children[char] = new TrieNode();
+            curr = curr.children[char];
+        }
+
+        curr.word = true;
+    }
+
+    /**
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word) {
+        function dfs(node, idx) {
+            let curr = node;
+
+            for(let i=idx; i < word.length; i++) {
+                const char = word[i];
+
+                if(char === ".") {
+                    for(let child of Object.values(curr.children)) {
+                        if(dfs(child, i + 1)) return true;
+                    }
+                    return false;
+                } else {
+                    if(!curr.children[char]) return false;
+                    curr = curr.children[char];
+                }
+            }
+            return curr.word;
+        }
+
+        return dfs(this.root, 0);
+    }
+}
