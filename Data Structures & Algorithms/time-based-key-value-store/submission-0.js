5995df4d64fc7d015cbc315ba2d27e614ddb2389
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-05-13 10:54:01 -0700

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.js
@@ -0,0 +1,40 @@
+class TimeMap {
+    constructor() {
+        this.keyStore = new Map();
+    }
+
+    /**
+     * @param {string} key
+     * @param {string} value
+     * @param {number} timestamp
+     * @return {void}
+     */
+    set(key, value, timestamp) {
+        if(!this.keyStore.has(key)) this.keyStore.set(key, []);
+        this.keyStore.get(key).push([value, timestamp]);
+    }
+
+    /**
+     * @param {string} key
+     * @param {number} timestamp
+     * @return {string}
+     */
+    get(key, timestamp) {
+        if(!this.keyStore.get(key)) return "";
+
+        const values = this.keyStore.get(key);
+
+        let lo = 0, hi = values.length - 1;
+
+        while(lo <= hi) {
+            const mid = Math.floor((lo + hi) / 2);
+
+            const time = values[mid][1];
+            if(time === timestamp) return values[mid][0];
+            if(time > timestamp) hi = mid - 1;
+            else lo = mid + 1;
+        }
+
+        return hi >= 0 ? values[hi][0] : "";
+    }
+}
