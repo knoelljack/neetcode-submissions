bdee0d99b5ced6b04ac48e5f8cb1ecd7bda36ed0
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-05-16 13:13:26 -0700

    Add: evaluate-reverse-polish-notation - submission-3

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-3.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+        const stack = [];
+
+        for(let token of tokens) {
+            if(!isNaN(token)) stack.push(Number(token));
+            else {
+                const b = stack.pop();
+                const a = stack.pop();
+
+                if(token === "*") stack.push(a * b);
+                else if (token === "/") stack.push(Math.trunc(a / b));
+                else if (token === "+") stack.push(a + b);
+                else stack.push(a - b);
+            }
+        }
+
+        return stack[0];
+    }
+}
