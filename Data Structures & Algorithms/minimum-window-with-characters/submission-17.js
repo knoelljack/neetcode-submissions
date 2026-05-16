2f0431e6f7a2c479704428a0ce9a545c79c6c7c7
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-05-15 23:25:32 -0700

    Add: minimum-window-with-characters - submission-17

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-17.js
@@ -0,0 +1,46 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {string}
+     */
+    minWindow(s, t) {
+        let resLen = Infinity;
+        let resIdx = [-1,-1];
+        const needed = {};
+        const window = {};
+        let have = 0;
+        
+        for(const char of t) {
+            needed[char] = (needed[char] || 0) + 1;
+        }
+
+        const need = Object.entries(needed).length;
+
+        let l = 0;
+        let r = 0;
+
+        while(r < s.length) {
+            const rightChar = s[r];
+            window[rightChar] = (window[rightChar] || 0) + 1;
+            
+            if(rightChar in needed && window[rightChar] === needed[rightChar]) have++;
+
+            while(have === need) {
+                if(r - l + 1 < resLen) {
+                    resLen = r - l + 1;
+                    resIdx = [l, r];
+                }
+
+                const leftChar = s[l];
+                if(leftChar in needed && window[leftChar] === needed[leftChar]) have--;
+                window[leftChar]--;
+                l++;
+            }
+
+            r++;
+        }
+
+        return resLen === Infinity ? "" : s.substring(resIdx[0], resIdx[1] + 1);
+    }
+}
