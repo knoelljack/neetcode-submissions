4a6a5b6ee36c038beeafce2699f4226bd775f998
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-05-22 08:10:39 -0700

    Add: palindromic-substrings - submission-12

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-12.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    countSubstrings(s) {
+        let count = 0;
+
+        function check(l, r) {
+            let res = 0;
+
+            while(s[r] && s[l] === s[r]) {
+                res++;
+                l--;
+                r++;
+            }
+
+            return res;
+        }
+
+        for(let i=0; i < s.length; i++) {
+            count += check(i, i);
+            count += check(i, i+1);
+        }
+
+        return count;
+    }
+}
