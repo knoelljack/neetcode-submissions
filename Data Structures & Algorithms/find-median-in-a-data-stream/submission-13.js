6c6752aceeb302d357482c3e89b9bd4c2712db92
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-05-22 14:49:40 -0700

    Add: find-median-in-a-data-stream - submission-13

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-13.js
@@ -0,0 +1,42 @@
+class MedianFinder {
+    constructor() {
+        this.small = new MaxPriorityQueue();
+        this.large = new MinPriorityQueue();
+    }
+
+    /**
+     *
+     * @param {number} num
+     * @return {void}
+     */
+    addNum(num) {
+        if(this.large.isEmpty() || num > this.large.front()) {
+            this.large.enqueue(num);
+        } else {
+            this.small.enqueue(num);
+        }
+
+        if(this.large.size() > this.small.size() + 1) {
+            this.small.enqueue(this.large.dequeue());
+        }
+
+        else if(this.small.size() > this.large.size() + 1) {
+            this.large.enqueue(this.small.dequeue());
+        }
+    }
+
+    /**
+     * @return {number}
+     */
+    findMedian() {
+        if(this.large.size() > this.small.size()) {
+            return this.large.front();
+        }
+
+        if(this.small.size() > this.large.size()) {
+            return this.small.front();
+        }
+
+        return (this.small.front() + this.large.front()) / 2;
+    }
+}
