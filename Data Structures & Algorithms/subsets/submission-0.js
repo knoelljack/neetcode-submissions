ebdfd5433a90aa10c086891afd76eabd5e92467b
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-05-25 16:55:17 -0700

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    subsets(nums) {
+        const res = [];
+
+        function dfs(idx, curr) {
+            if(idx === nums.length) {
+                res.push([...curr]);
+                return;
+            }
+
+            curr.push(nums[idx]);
+            dfs(idx + 1, curr);
+            curr.pop();
+            dfs(idx + 1, curr);
+        }
+
+        dfs(0, []);
+        return res;
+    }
+}
