e49bbb41504ebfa4361309087f118d2dbc4e9b31
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-05-26 16:26:59 -0700

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    permute(nums) {
+        const res = [];
+
+        function dfs(start) {
+
+            if(start === nums.length) {
+                res.push([...nums]);
+                return;
+            }
+
+            for(let i=start; i < nums.length; i++) {
+                [nums[start], nums[i]] = [nums[i], nums[start]];
+
+                dfs(start+1);
+
+                [nums[i], nums[start]] = [nums[start], nums[i]];
+            }
+        }
+
+        dfs(0);
+        return res;
+    }
+}
