49c18c80c012b82b3977d25a70b1270699909b2a
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-05-27 11:30:10 -0700

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.js
@@ -0,0 +1,44 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+
+        const currMin = this.minStack[this.minStack.length - 1];
+
+        if(!this.minStack.length || val < currMin) {
+            this.minStack.push(val);
+        } else {
+            this.minStack.push(currMin);
+        }
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop();
+        this.minStack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
