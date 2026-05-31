1b524adf29d318a23ff5f98429cdd8b5400a52c4
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-05-31 15:16:24 -0700

    Add: course-schedule - submission-15

---
+++ Data Structures & Algorithms/course-schedule/submission-15.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {boolean}
+     */
+    canFinish(numCourses, prerequisites) {
+        const visited = new Set();
+        const crsMap = {};
+
+        for(let i=0; i < numCourses; i++) {
+            crsMap[i] = [];
+        }
+
+        for(let [crs, pre] of prerequisites) {
+            crsMap[crs].push(pre);
+        }
+
+        function dfs(crs) {
+            if(visited.has(crs)) return false;
+            if(crsMap[crs] === []) return true;
+            visited.add(crs);
+
+            for(let pre of crsMap[crs]) {
+                if(!dfs(pre)) return false;
+            }
+
+            visited.delete(crs);
+            crsMap[crs] === [];
+            return true;
+        }
+
+        for(let i=0; i < numCourses; i++) {
+            if(!dfs(i)) return false;
+        }
+
+        return true;
+    }
+}
