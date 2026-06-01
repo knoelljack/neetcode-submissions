3501f4b576361feadc66d2266b7949d4757b5587
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-05-31 19:11:20 -0700

    Add: valid-tree - submission-20

---
+++ Data Structures & Algorithms/valid-tree/submission-20.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {boolean}
+     */
+    validTree(n, edges) {
+        const adj = Array.from({length: n}, () => []);
+        const visited = new Set();
+
+        for(let [n1,n2] of edges) {
+            adj[n1].push(n2);
+            adj[n2].push(n1);
+        }
+
+        function graph(node, parent) {
+            if(visited.has(node)) return false;
+            visited.add(node);
+
+            for(let nei of adj[node]) {
+                if(nei === parent) continue;
+                if(!graph(nei, node)) return false;
+            }
+
+            return true;
+        }
+
+        return graph(0, -1) && visited.size === n;
+    }
+}
