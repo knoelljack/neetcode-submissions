c46fd94e42fca0bd0ca778d464df970a38ba74cf
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-06-02 09:49:00 -0700

    Add: string-encode-and-decode - submission-21

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-21.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let res = "";
+
+        for(let str of strs) {
+            res += str.length + "$" + str;
+        }
+
+        return res;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        const res = [];
+
+        let i = 0;
+
+        while(i < str.length) {
+            let j = i;
+
+            while(str[j] !== "$") j++;
+
+            const len = parseInt(str.substring(i, j));
+            const s = str.substring(j+1, j + len + 1);
+
+            res.push(s);
+            i = j + len + 1;
+        }
+
+        return res;
+    }
+}
