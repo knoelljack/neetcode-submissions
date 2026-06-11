8e34af4de6a0095c678ad438e94450798e7ca339
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-06-11 09:30:26 -0700

    Add: count-good-nodes-in-binary-tree - submission-3

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-3.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    goodNodes(root) {
+        
+        function dfs(node, maxSoFar) {
+            if(!node) return 0;
+
+            let count = 0;
+
+            if(node.val >= maxSoFar) {
+                count = 1;
+            }
+
+            maxSoFar = Math.max(maxSoFar, node.val);
+
+            count += dfs(node.left, maxSoFar);
+            count += dfs(node.right, maxSoFar);
+
+            return count;
+        }
+
+        return dfs(root, -Infinity);
+    }
+}
