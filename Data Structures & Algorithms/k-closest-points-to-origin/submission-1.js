92484da7dd4f4467eb86d7bc095c65635f806c78
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-06-12 16:30:45 -0700

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[][]} points
+     * @param {number} k
+     * @return {number[][]}
+     */
+    kClosest(points, k) {
+        const dist = (p) => p[0] * p[0] + p[1] * p[1];
+
+        const minHeap = new MinPriorityQueue((p) => dist(p));
+
+        for(let point of points) {
+            minHeap.enqueue(point);
+        }
+
+        const res = [];
+
+        for(let i=0 ; i < k; i++) {
+            res.push(minHeap.dequeue());
+        }
+
+        return res;
+    }
+}
