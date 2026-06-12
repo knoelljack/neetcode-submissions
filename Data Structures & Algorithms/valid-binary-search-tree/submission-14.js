cd645bf50a13c45f126ac9fdcecf481749e3dd8a
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-06-11 17:43:55 -0700

    Add: valid-binary-search-tree - submission-14

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-14.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isValidBST(root) {
+        return this.isValid(root, -Infinity, Infinity);
+    }
+
+    isValid(node, min, max) {
+        if(!node) return true;
+        if(node.val <= min || node.val >= max) return false;
+        return this.isValid(node.left, min, node.val) && this.isValid(node.right, node.val, max);
+    }
+}
