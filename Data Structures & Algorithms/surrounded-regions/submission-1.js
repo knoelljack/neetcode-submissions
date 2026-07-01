7e17d9f244d0b7c9b80b134377c438e446176f4a
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-07-01 08:59:36 -0700

    Add: surrounded-regions - submission-1

---
+++ Data Structures & Algorithms/surrounded-regions/submission-1.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {void} Do not return anything, modify board in-place instead.
+     */
+    solve(board) {
+        const ROWS = board.length;
+        const COLS = board[0].length;
+
+        function dfs(r, c) {
+            if(r < 0 || c < 0 || r >= ROWS || c >= COLS || board[r][c] !== "O") return;
+            board[r][c] = "T";
+            dfs(r+1, c);
+            dfs(r-1, c);
+            dfs(r, c+1);
+            dfs(r, c-1);
+        }
+
+        for(let r=0; r < ROWS; r++) {
+            dfs(r, 0);
+            dfs(r, COLS-1);
+        }
+
+        for(let c=0; c < COLS; c++) {
+            dfs(0, c);
+            dfs(ROWS-1, c);
+        }
+
+        for(let r=0; r < ROWS; r++) {
+            for(let c=0; c < COLS; c++) {
+                if(board[r][c] === "O") {
+                    board[r][c] = "X";
+                } else if (board[r][c] === "T") {
+                    board[r][c] = "O";
+                }
+            }
+        }
+    }
+}
