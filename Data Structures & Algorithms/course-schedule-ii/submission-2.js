7b2ad519a0b307c3aa35b9b763f0eb2f94b20682
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-07-01 12:54:02 -0700

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {number[]}
+     */
+    findOrder(numCourses, prerequisites) {
+        const crsMap = {};
+        const visiting = new Set();
+        const visited = new Set();
+        const res = [];
+
+        for(let i=0; i < numCourses; i++) {
+            crsMap[i] = [];
+        }
+
+        for(let [crs,pre] of prerequisites) {
+            crsMap[crs].push(pre);
+        }
+
+        function dfs(crs) {
+            if(visiting.has(crs)) return false;
+            if(visited.has(crs)) return true;
+
+            visiting.add(crs);
+
+            for(let pre of crsMap[crs]) {
+                if(!dfs(pre)) return false;
+            }
+
+            visiting.delete(crs);
+            visited.add(crs);
+            res.push(crs);
+            return true;
+        }
+
+        for(let i=0; i < numCourses; i++) {
+            if(!dfs(i)) return [];
+        }
+
+        return res;
+    }
+}
