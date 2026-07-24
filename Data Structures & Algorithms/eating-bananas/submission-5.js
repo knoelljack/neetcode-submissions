3b4c36dd6ecc0854d384a626fbd216795998a773
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-07-24 09:25:52 -0700

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        let lo = 0, hi = Math.max(...piles);
+        let res = 0;
+
+        while(lo <= hi) {
+            const k = Math.floor((lo + hi) / 2);
+
+            let hours = 0;
+
+            for(let pile of piles) {
+                hours += Math.ceil(pile / k);
+            }
+
+            if(hours <= h) {
+                res = k;
+                hi = k - 1;
+            } else {
+                lo = k + 1;
+            }
+        }
+
+        return res;
+    }
+}
