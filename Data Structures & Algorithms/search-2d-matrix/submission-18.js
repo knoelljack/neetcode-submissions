5a96141939759365a6d584f364655e8955b81f09
Jack Knoell <89558885+knoelljack@users.noreply.github.com>
2026-09-18 11:30:22 -0700

    Add: search-2d-matrix - submission-18

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-18.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        let currRow = 0;
+
+        while(currRow < matrix.length) {
+            let lo = 0, hi = matrix[currRow].length - 1;
+
+            if(target < matrix[currRow][lo]) return false;
+            else if(target > matrix[currRow][hi]) {
+                currRow++;
+                continue;
+            }
+            else {
+                while(lo <= hi) {
+                    const mid = Math.floor((lo + hi) / 2);
+                    if(matrix[currRow][mid] === target) return true;
+                    else if (target > matrix[currRow][mid]) lo = mid + 1;
+                    else hi = mid - 1;
+                }
+            }
+            return false;
+        }
+
+        return false;
+    }
+}
